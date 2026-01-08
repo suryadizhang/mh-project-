7594460298d234265c07a47b7dbf6a2606520b8f
Surya <193637108+suryadizhang@users.noreply.github.com>
2026-01-07 23:02:44 -0800

    fix(auth): read email from JWT payload instead of sub (user ID)

---
+++ test_initial_login.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s -X POST http://localhost:8000/api/auth/login \
+  -H 'Content-Type: application/json' \
+  -d '{"email":"suryadizhang.swe@gmail.com","password":"***REDACTED***"}'
---
+++ test_station_login.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s -X POST "http://127.0.0.1:8000/api/station/station-login" \
+  -H "Content-Type: application/json" \
+  -d '{"email":"suryadizhang.swe@gmail.com","password":"***REDACTED***"}'
